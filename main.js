cd0ddf002836e2813b47423f72d09d3bcbd3e621
Steven Allen <steven@stebalien.com>
2017-09-26 09:31:55 +0300

    Update docs for d3af758decec8dc63a317c28a400b4e8db8f992f
    Release 2.2.0
    
    * Adds Redox support.

--- main.js
+++ main.js
@@ -1065,6 +1065,7 @@
         block("macro", "Macros");
         block("struct", "Structs");
         block("enum", "Enums");
+        block("union", "Unions");
         block("constant", "Constants");
         block("static", "Statics");
         block("trait", "Traits");
@@ -1270,7 +1271,7 @@
                     e.innerHTML = labelForToggleButton(true);
                 });
                 onEach(toggle.getElementsByClassName('toggle-label'), function(e) {
-                    e.style.display = 'block';
+                    e.style.display = 'inline-block';
                 });
             }
         }
@@ -1294,6 +1295,24 @@
             i_e.parentNode.insertBefore(createToggleWrapper(), i_e);
             collapseDocs(i_e.previousSibling.childNodes[0]);
         });
+    });
+
+    onEach(document.getElementsByClassName('rust-example-rendered'), function(e) {
+        if (hasClass(e, 'compile_fail')) {
+            e.addEventListener("mouseover", function(event) {
+                e.previousElementSibling.childNodes[0].style.color = '#f00';
+            });
+            e.addEventListener("mouseout", function(event) {
+                e.previousElementSibling.childNodes[0].style.color = '';
+            });
+        } else if (hasClass(e, 'ignore')) {
+            e.addEventListener("mouseover", function(event) {
+                e.previousElementSibling.childNodes[0].style.color = '#ff9200';
+            });
+            e.addEventListener("mouseout", function(event) {
+                e.previousElementSibling.childNodes[0].style.color = '';
+            });
+        }
     });
 }());
 
